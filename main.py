536cad661ef6908036d7a98d43ac2f6e96311347
hexstories <kwakuasihene@gmail.com>
2024-05-01 17:17:16 +0000

    bible api

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run(
+   uvicorn.run(
         app = "app.main:app",
         host = "127.0.0.1",
         port = 8000,
         reload = True
     )
+
+
